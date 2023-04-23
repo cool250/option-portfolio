0bb9e34ae313361f204a9640b03a4bd64b5b58c6
Nishant Usapkar <cool250@outlook.com>
2023-04-22 23:28:35 -0700

    adding host

--- index.py
+++ index.py
@@ -51,4 +51,4 @@ def render_page_content(pathname):
 
 port = int(os.environ.get('PORT', 8080)) 
 if __name__ == '__main__':
-    app.run(debug=True, port=port)
+    app.run(host='0.0.0.0', debug=True, port=port)
