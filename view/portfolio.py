44c07f3996267961759819407421e697b5aac9d7
Nishant Usapkar <cool250@outlook.com>
2023-04-30 11:40:40 -0700

    onload populate portfolio changes

--- view/portfolio.py
+++ view/portfolio.py
@@ -9,15 +9,6 @@ from utils.functions import formatter_currency
 
 layout = dbc.Container(
     [
-        html.Div(
-            dbc.Button(
-                "Show Positions",
-                color="primary",
-                id="portfolio-btn",
-                className="me-md-2",
-            ),
-            className="d-md-flex justify-content-md-end",
-        ),
         dbc.Row(html.H4(children="PUTS")),
         html.Hr(className="my-2"),
         dbc.Row(
@@ -83,56 +74,54 @@ layout = dbc.Container(
         Output("stock-detail", "children"),
     ],
     [
-        Input("portfolio-btn", "n_clicks"),
+       [Input('url', 'pathname')],
     ],
 )
 def on_button_click(n):
-    if n is not None:
-        positions = AccountPositions()
+    positions = AccountPositions()
 
-        df_puts = positions.get_put_positions()
-        df_calls = positions.get_call_positions()
-        df_stocks = positions.get_stock_positions()
-        cash_required = formatter_currency(df_puts["COST"].sum())
-        stock_value = formatter_currency(
-            (df_stocks["TICKER PRICE"] * df_stocks["QTY"]).sum()
-        )
-        stock_cost = formatter_currency(
-            (df_stocks["AVG PRICE"] * df_stocks["QTY"]).sum()
-        )
+    df_puts = positions.get_put_positions()
+    df_calls = positions.get_call_positions()
+    df_stocks = positions.get_stock_positions()
+    cash_required = formatter_currency(df_puts["COST"].sum())
+    stock_value = formatter_currency(
+        (df_stocks["TICKER PRICE"] * df_stocks["QTY"]).sum()
+    )
+    stock_cost = formatter_currency(
+        (df_stocks["AVG PRICE"] * df_stocks["QTY"]).sum()
+    )
 
-        calls_dt = (
-            dash_tabulator.DashTabulator(
-                id="call-table",
-                columns=[{"id": i, "title": i, "field": i} for i in df_calls.columns],
-                data=df_calls.to_dict("records"),
-            ),
-        )
+    calls_dt = (
+        dash_tabulator.DashTabulator(
+            id="call-table",
+            columns=[{"id": i, "title": i, "field": i} for i in df_calls.columns],
+            data=df_calls.to_dict("records"),
+        ),
+    )
 
-        stocks_dt = (
-            dash_tabulator.DashTabulator(
-                id="put-table",
-                columns=[{"id": i, "title": i, "field": i} for i in df_stocks.columns],
-                data=df_stocks.to_dict("records"),
-            ),
-        )
+    stocks_dt = (
+        dash_tabulator.DashTabulator(
+            id="put-table",
+            columns=[{"id": i, "title": i, "field": i} for i in df_stocks.columns],
+            data=df_stocks.to_dict("records"),
+        ),
+    )
 
-        puts_dt = (
-            dash_tabulator.DashTabulator(
-                id="put-table",
-                columns=[{"id": i, "title": i, "field": i} for i in df_puts.columns],
-                data=df_puts.to_dict("records"),
-            ),
-        )
+    puts_dt = (
+        dash_tabulator.DashTabulator(
+            id="put-table",
+            columns=[{"id": i, "title": i, "field": i} for i in df_puts.columns],
+            data=df_puts.to_dict("records"),
+        ),
+    )
 
-        return (
-            puts_dt,
-            calls_dt,
-            stocks_dt,
-            True,
-            f" Put Exposure : {cash_required}",
-            True,
-            f" Stock Value : {stock_value}  Stock Cost : {stock_cost}",
-        )
-    else:
-        return None, None, None, None, None, None, None
+    return (
+        puts_dt,
+        calls_dt,
+        stocks_dt,
+        True,
+        f" Put Exposure : {cash_required}",
+        True,
+        f" Stock Value : {stock_value}  Stock Cost : {stock_cost}",
+    )
+
