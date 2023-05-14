b192e6377503c5cbb9877e3289c1c8bcd616261b
Nishant Usapkar <cool250@outlook.com>
2023-05-14 11:17:56 -0700

    Analysis screen

--- view/analysis.py
+++ view/analysis.py
@@ -1,7 +1,11 @@
 import dash_bootstrap_components as dbc
-import json
+import pandas as pd
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
+import dash_tabulator
+import plotly.graph_objects as go
+from opstrat import multi_plotter
+import numpy as np
 
 from app import app
 
@@ -14,8 +18,8 @@ TOP_COLUMN = dbc.Form(
                         dbc.Label("Option Type", size="sm"),
                         dbc.Select(
                             options=[
-                                {"label": "PUT", "value": "PUT"},
-                                {"label": "CALL", "value": "CALL"},
+                                {"label": "PUT", "value": "p"},
+                                {"label": "CALL", "value": "c"},
                             ],
                             value="",
                             id="a_contractType",
@@ -23,15 +27,15 @@ TOP_COLUMN = dbc.Form(
                             size="sm",
                         ),
                     ],
-                    width=2,
+                    width=1,
                 ),
                 dbc.Col(
                     children=[
-                        dbc.Label("Transaction Type", size="sm"),
+                        dbc.Label("Transaction", size="sm"),
                         dbc.Select(
                             options=[
-                                {"label": "SELL", "value": "SELL"},
-                                {"label": "BUY", "value": "BUY"},
+                                {"label": "SELL", "value": "s"},
+                                {"label": "BUY", "value": "b"},
                             ],
                             value="",
                             id="a_tran_type",
@@ -39,7 +43,7 @@ TOP_COLUMN = dbc.Form(
                             size="sm",
                         ),
                     ],
-                    width=2,
+                    width=1,
                 ),
                 dbc.Col(
                     children=[
@@ -47,11 +51,47 @@ TOP_COLUMN = dbc.Form(
                         dbc.Input(
                             type="text",
                             id="a_ticker",
-                            placeholder="Enter Ticker",
+                            placeholder="",
                             size="sm",
                         ),
                     ],
-                    width=2,
+                    width=1,
+                ),
+                dbc.Col(
+                    children=[
+                        dbc.Label("Premium", size="sm"),
+                        dbc.Input(
+                            type="text",
+                            id="a_premium",
+                            placeholder="",
+                            size="sm",
+                        ),
+                    ],
+                    width=1,
+                ),
+                dbc.Col(
+                    children=[
+                        dbc.Label("Strike", size="sm"),
+                        dbc.Input(
+                            type="text",
+                            id="a_strike",
+                            placeholder="",
+                            size="sm",
+                        ),
+                    ],
+                    width=1,
+                ),
+                   dbc.Col(
+                    children=[
+                        dbc.Label("Lot Size", size="sm"),
+                        dbc.Input(
+                            type="text",
+                            id="a_lot",
+                            placeholder="",
+                            size="sm",
+                        ),
+                    ],
+                    width=1,
                 ),
                 dbc.Col(
                     children=[
@@ -62,70 +102,111 @@ TOP_COLUMN = dbc.Form(
                             className="mt-4",
                         ),
                     ],
-                    width=2,
-                ),
-                dbc.Col(
-                    children=[
-                        dbc.Button(
-                            "Analyze",
-                            color="primary",
-                            id="analysis-btn",
-                            className="mt-4",
-                        ),
-                    ],
-                    width=2,
+                    width=1,
                 ),
             ],
         ),
     ],
 )
 
-SEARCH_RESULT = html.Div(id="a_content")
+STRATEGY_LIST = dbc.Row(
+    children=[
+        dbc.Col(
+            children=[
+                html.Div(id="a_content"),
+            ]
+        ),
+        dbc.Col(
+            children=[
+                dbc.Button(
+                    "Analyze",
+                    color="primary",
+                    id="analysis-btn",
+                    className="mt-4",
+                    style=dict(display="none"),
+                ),
+            ]
+        ),
+    ]
+)
+
+STRATEGY_CHART = dbc.Row(
+    children=[
+        dcc.Graph(id="a_graph"),
+    ]
+)
 
 layout = dbc.Container(
     [
         dbc.Row(TOP_COLUMN),
         html.P(),
-        dbc.Row(SEARCH_RESULT),
+        dbc.Row(STRATEGY_LIST),
+        html.P(),
+        dbc.Row(STRATEGY_CHART),
         # dcc.Store stores the intermediate value
-        dcc.Store(id='cache_data')
+        dcc.Store(id="cache_data"),
     ],
 )
 
 
 @app.callback(
-    Output('cache_data', 'data'),
+    Output("cache_data", "data"),
+    Output("a_content", "children"),
+    Output("analysis-btn", "style"),
     [Input("add-btn", "n_clicks")],
     [
         State("a_contractType", "value"),
         State("a_tran_type", "value"),
         State("a_ticker", "value"),
-        State('cache_data', 'data'),
+        State("a_premium", "value"),
+        State("a_lot", "value"),
+        State("a_strike", "value"),
+        State("cache_data", "data"),
     ],
 )
-def on_add_click(n, a_contractType, a_tran_type, ticker, cache_data):
+def on_add_click(n, op_type, tr_type, ticker, op_pr, contract, strike, cache_data):
     if n is None:
-        return []
+        return [], None, dict(display="none")
     else:
-        contract_obj = {'a_contractType': a_contractType, 'a_tran_type': a_tran_type, 'ticker': ticker}
+        contract_obj = {
+            "op_type": op_type,
+            "tr_type": tr_type,
+            "ticker": ticker,
+            "op_pr": int(op_pr),
+            "contract": int(contract),
+            "strike": int(strike),
+        }
         cache_obj = cache_data
         if not cache_obj:
             cache_obj = []
         cache_obj.append(contract_obj)
-        return cache_obj
-    
+        # Display the strategies selected
+        df = pd.DataFrame.from_dict(cache_obj)
+        dt = (
+            dash_tabulator.DashTabulator(
+                id="analysis-table",
+                columns=[{"id": i, "title": i, "field": i} for i in df.columns],
+                data=df.to_dict("records"),
+            ),
+        )
+        return cache_obj, dt, dict()
+
 
 @app.callback(
-    Output('a_content', 'children'),
+    Output("a_graph", "figure"),
     [Input("analysis-btn", "n_clicks")],
     [
-        State('cache_data', 'data'),
+        State("cache_data", "data"),
     ],
 )
 def on_analyze_click(n, cache_data):
     if n is None:
-        return ""
+        return None
     else:
-        cache_obj = cache_data
-        return cache_obj
-    
+        spot = 410
+        # fig = multi_plotter(spot=spot, op_list=cache_data)
+        op_1 = {'op_type':'c','strike':420,'tr_type':'s','op_pr':3.27}
+        op_2 = {'op_type':'p','strike':400,'tr_type':'s','op_pr':3.02}
+        op_3 = {'op_type':'c','strike':425,'tr_type':'b','op_pr':1.53}
+        fig = multi_plotter(spot=412, op_list=[op_1,op_2, op_3])
+        return fig
