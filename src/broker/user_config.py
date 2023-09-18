718bf7bce9cd38e254d4ce4e21dd44c278eb7e46
Nishant Usapkar <cool250@outlook.com>
2023-09-17 20:41:35 -0700

    1) New JSON based account configurator 2) stylesheet changes 3) New venv naming convention added to .gitignore

--- src/broker/user_config.py
+++ src/broker/user_config.py
@@ -1,10 +1,10 @@
 import os
 
-from utils.config import ConfigManager
+from utils.accounts import Accounts
 
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 
 
 class UserConfig:
-    ACCOUNT_NUMBER = ConfigManager.getInstance().getConfig("primary", "ACCOUNT_NUMBER")
-    CONSUMER_ID = ConfigManager.getInstance().getConfig("primary", "CONSUMER_ID")
+    ACCOUNT_NUMBER = Accounts().get_account_number("brokerage")
+    CONSUMER_ID = Accounts().get_consumer_id("brokerage")
